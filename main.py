59612df8436454ddde763a8d67c5f29c4704f06c
q <q@localhost.my.domain>
2025-06-26 14:38:15 +0300

    Add my lab work

--- main.py
+++ main.py
@@ -1,72 +1,45 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
-app = Flask('Furniture store')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-# products = [
-#     {'prod_name': 'sofa',
-#      'price': 12000,
-#      'in_stock': False,
-#      'id': 0},
-#     {'prod_name': 'table',
-#      'price': 6000,
-#      'in_stock': True,
-#      'id': 1},
-#     {'prod_name': 'chair',
-#      'price': 8000,
-#      'in_stock': False,
-#      'id': 2},
-# ]
-
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    prod_name = db.Column(db.String(300))
+    hardware_part = db.Column(db.String(300), nullable=False)
     price = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
-
-    def __repr__(self):
-        return f'Product{self.id}. {self.prod_name} - {self.price} rub.'
-
 
 @app.route('/')
 def main():
-    products = Product.query.all()
-    return render_template('index.html', products_list=products)
-
-
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    product = Product.query.get(product_id)
-    product.in_stock = request.json['in_stock']
-    db.session.commit()
-    # global products
-    # in_stock = request.json['in_stock']
-    # for product in products:
-    #     if product['id'] == product_id:
-    #         product.update({'in_stock': in_stock})
-    # return 'OK'
+    products_list = Product.query.all()
+    return render_template('index.html',
+       products_list=products_list,
+       products_list_length = len(products_list))
 
 
 @app.route('/add', methods=['POST'])
 def add_product():
     data = request.json
-    product = Product(**data)
-    db.session.add(product)
+    db.session.add(Product(**data))
     db.session.commit()
+    return {'message': 'Product added'}
 
-    # id_last = products[-1]['id']
-    # id_new = id_last + 1
-    # data['id'] = id_new
-    # products.append(data)
-    return 'OK'
 
+@app.route('/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    db.session.delete(Product.query.get(product_id))
+    db.session.commit()
+    return {'message': 'Product deleted'}
+
+@app.route('/delete', methods=['DELETE'])
+def delete_all_products():
+    db.session.query(Product).delete()
+    db.session.commit()
+    return {'message': 'Products deleted'}
 
 
 if __name__ == '__main__':
--- static/update.js
+++ static/update.js
@@ -1,22 +1,34 @@
-function updateProd(el) {
-    product_id = el.value
-    fetch('/in_stock/' + product_id, {
-        method: 'patch',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'in_stock': el.checked})
-    })
-    console.log(product_id)
+function deleteProduct(productId) {
+  fetch(`/delete/${productId}`, { method: 'DELETE' }).then(response => {
+    if (response.ok) {
+      location.reload()
+    }
+  })
 }
 
-function addProduct() {
-    let prodName = document.getElementById('prod_name').value
-    let price = document.getElementById('price').value
-    fetch('/add', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'prod_name': prodName,
-                             'price': price,
-                             'in_stock': true})
+function addProduct(event) {
+  event.preventDefault()
+  let prodName = document.getElementById('hardware_part').value
+  let price = document.getElementById('price').value
+  fetch('/add', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'hardware_part': prodName,
+      'price': price
     })
-//    console.log("Add")
+  })
+  .then(response => {
+    if (response.ok) {
+      location.reload()
+    }
+  })
+}
+
+function deleteAllProducts() {
+  fetch('/delete', { method: 'DELETE' }).then(response => {
+    if (response.ok) {
+      location.reload()
+    }
+  })
 }
